47a613c19ec27c376caed858a03ce25baa6d9252
frogro <frankiin77@hotmail.com>
2025-08-29 19:24:41 +0200

    Update cs8409_compat.h

--- cirruslogic/cs8409_compat.h
+++ cirruslogic/cs8409_compat.h
@@ -1,61 +1,52 @@
-/* cs8409_compat.h – vereinheitlichte Includes für HDA/ALSA Header
- * Funktioniert auf Debian/Ubuntu/Fedora/Arch, Kernel 5.x–6.x
+/* cs8409_compat.h
+ *
+ * Zentrale Kompatibilitäts-Includes für HDA/ALSA-Header.
+ * Nutzt __has_include (gcc) um die passenden Kernel-Header-Pfade
+ * je nach Distribution/Kernellayout zu finden.
+ *
+ * Einbinden: einfach NUR diese Datei in deinen .c/.h verwenden:
+ *   #include "cs8409_compat.h"
  */
+
 #ifndef CS8409_COMPAT_H
 #define CS8409_COMPAT_H
 
-/* Erst normale Linux/Sound-Basis (idR. schon in .c vorhanden) – hier neutral lassen */
-/* #include <linux/...>  #include <sound/...>  -> verbleibt in den .c-Dateien */
+/* __has_include ist bei aktuellen gcc/clang verfügbar */
+#ifndef __has_include
+  #define __has_include(x) 0
+#endif
 
-/* HDA-Header – portable Auflösung */
-#if defined(__has_include)
-  /* 1) lokaler Header (ältere Out-of-tree Treiber) */
-  #if __has_include("hda_local.h")
-    #include "hda_local.h"
-  /* 2) üblicher Kernel-Pfad */
-  #elif __has_include(<sound/pci/hda/hda_local.h>)
-    #include <sound/pci/hda/hda_local.h>
-  #else
-    #error "hda_local.h nicht gefunden – Kernel-Header (linux-headers-$(uname -r)) installieren?"
-  #endif
-
-  #if __has_include("hda_codec.h")
-    #include "hda_codec.h"
-  #elif __has_include(<sound/pci/hda/hda_codec.h>)
-    #include <sound/pci/hda/hda_codec.h>
-  #endif
-
-  #if __has_include("hda_jack.h")
-    #include "hda_jack.h"
-  #elif __has_include(<sound/pci/hda/hda_jack.h>)
-    #include <sound/pci/hda/hda_jack.h>
-  #endif
-
-  #if __has_include("hda_auto_parser.h")
-    #include "hda_auto_parser.h"
-  #elif __has_include(<sound/pci/hda/hda_auto_parser.h>)
-    #include <sound/pci/hda/hda_auto_parser.h>
-  #endif
-
-  #if __has_include("hda_generic.h")
-    #include "hda_generic.h"
-  #elif __has_include(<sound/pci/hda/hda_generic.h>)
-    #include <sound/pci/hda/hda_generic.h>
-  #endif
-
-  #if __has_include("hda_bind.h")
-    #include "hda_bind.h"
-  #elif __has_include(<sound/pci/hda/hda_bind.h>)
-    #include <sound/pci/hda/hda_bind.h>
-  #endif
-#else
-  /* Fallback ohne __has_include: Standard-Kernelpfad */
+/* Häufigster Fall (Debian/Ubuntu/Fedora/Arch neuere Kernel):
+ * Header liegen unter sound/pci/hda/
+ */
+#if __has_include(<sound/pci/hda/hda_local.h>)
   #include <sound/pci/hda/hda_local.h>
   #include <sound/pci/hda/hda_codec.h>
   #include <sound/pci/hda/hda_jack.h>
   #include <sound/pci/hda/hda_auto_parser.h>
-  #include <sound/pci/hda/hda_generic.h>
   #include <sound/pci/hda/hda_bind.h>
+  #include <sound/pci/hda/hda_generic.h>
+
+/* Manche ältere Bäume nutzten sound/ statt sound/pci/hda/ */
+#elif __has_include(<sound/hda_local.h>)
+  #include <sound/hda_local.h>
+  #include <sound/hda_codec.h>
+  #include <sound/hda_jack.h>
+  #include <sound/hda_auto_parser.h>
+  #include <sound/hda_bind.h>
+  #include <sound/hda_generic.h>
+
+/* Fallback: In-Tree-Builds (oder wenn der Treiber im Kernelbaum liegt) */
+#elif __has_include("hda_local.h")
+  #include "hda_local.h"
+  #include "hda_codec.h"
+  #include "hda_jack.h"
+  #include "hda_auto_parser.h"
+  #include "hda_bind.h"
+  #include "hda_generic.h"
+
+#else
+  #error "Kein passender Pfad für HDA-Header gefunden. Prüfe Kernel-Header-Installation."
 #endif
 
 #endif /* CS8409_COMPAT_H */
